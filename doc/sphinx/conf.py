7942c0b10a42873d3507cac095a14b6aa9a73cdc
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-16 15:58:27 -0400

    Add sphinx documentation.

---
+++ doc/sphinx/conf.py
@@ -0,0 +1,13 @@
+project = 'BER'
+copyright = '2020, Jeff Caffrey-Hill'
+author = 'Jeff Caffrey-Hill'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_css_files = ['css/style.css']
+html_theme_options = {
+    'nosidebar': True,
+ }
